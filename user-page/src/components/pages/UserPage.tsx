a066276e25a9d33a1b7fcbd51ff2f97cd590ee74
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-10-03 02:07:28 +0300

    release v1.0.0 UserPage (#11)
    * 1 was created default structure of react app project
    
    * create header component
    
    * create footer component
    
    * rewrite bootstrap default styles for list
    
    * Dev functional components (#10)
    
    * Установлены необходимые модули для корректной работы React и TypeScript
    
    * Добавлен компонент UserData для отображения данных пользователя
    
    * Удалены файлы .gitignore, LICENSE, README.md и добавлены компоненты UserInfo, Courses и DownloadFiles
    
    * Восстановлены файлы .gitignore, LICENSE и README.md
    
    * Добавлены блоки: заголовок с картинками, информация о пользователе, курсы - для дизайна
    
    * Добавлены поля для получения данных User
    
    * Has been done addtionaly settings of axios requests
    
    ---------
    
    Co-authored-by: Alla Pavlova <Pavlo_fo95@student.itstep.org>
    Co-authored-by: Pavlo-fo95 <167747709+Pavlo-fo95@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Borodi21 <Borod_yi21@student.itstep.org>
    Co-authored-by: anfishka <97487005+anfishka@users.noreply.github.com>
    Co-authored-by: Alla Pavlova <Pavlo_fo95@student.itstep.org>
    Co-authored-by: Pavlo-fo95 <167747709+Pavlo-fo95@users.noreply.github.com>

---
+++ user-page/src/components/pages/UserPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserData from '../../components/UserData';
+
+const UserPage = () => {
+  return (
+    <div>
+      <h1>Информация о пользователе</h1>
+      <UserData />
+    </div>
+  );
+};
+
+export default UserPage;
